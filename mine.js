5057f987c57c7f2e9500dba7f86f0bffee02cd09
hyon3000 <fwe9849@naver.com>
2023-03-25 21:27:24 +0900

    Add files via upload

--- mine.js
+++ mine.js
@@ -109,10 +109,8 @@
         if (this.mines[x][y] < this.max_mines) {
           n_max = this.max_mines - this.mines[x][y];
           n_max = Math.min(n_max, this.num_mines - num_mine_created);
-          if(this.max_mines==4 && this.num_mines>300) n = Math.floor(Math.random() * (n_max+2));
+          if(this.num_mines - num_mine_created>220) n = n_max;
           else n = Math.floor(Math.random() * n_max) + 1;
-          if(n>n_max) n=n_max; 
-          else if(n==0) n=1;
           if (this.mines[x][y] === 0) {
             this.remaining -= 1;
           }
