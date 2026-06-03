d72baa97c96b2c08a57db3a6706e121b09fd653f
a <fwe9849@naver.com>
2026-06-03 15:23:13 +0900

    a

--- polycube/web/app.js
+++ polycube/web/app.js
@@ -505,7 +505,12 @@ function initBlockState() {
   state.nextblock = state.b[1];
   state.holdblock = state.b[3];
   clear3d(state.holdblock, 0);
-  state.holdblock[3][3][3] = itemsEnabled ? (randInt(2) === 0 ? 19 : 18) : 65;
+  if (itemsEnabled) {
+    let _hv = 4;
+    if (randInt(10) === 0) { _hv = [1,30,31][randInt(3)]; }
+    else { const _u = randInt(1000000); if(_u<100)_hv=116;else if(_u<400)_hv=117;else if(_u<700)_hv=118;else if(_u<720)_hv=119;else if(_u<1520)_hv=104;else if(_u<2020)_hv=120;else if(_u<3020)_hv=121;else if(_u<3720)_hv=122;else if(_u<4020)_hv=123;else if(_u<4070)_hv=124;else if(_u<4870)_hv=125;else if(_u<5120)_hv=91;else if(_u<5220)_hv=102;else if(_u<5420)_hv=126;else if(_u<5620)_hv=105;else if(_u<5920)_hv=127;else if(_u<6020)_hv=17;else if(_u<6220)_hv=20;else if(_u<6620)_hv=21;else if(_u<7420)_hv=22;else if(_u<7670)_hv=16;else if(_u<7870)_hv=11;else if(_u<8120)_hv=2;else if(_u<9120)_hv=8;else if(_u<10120)_hv=9;else if(_u<10370)_hv=10;else if(_u<11370)_hv=5;else if(_u<11620)_hv=6;else if(_u<13870)_hv=120;else if(_u<24170)_hv=200;else if(_u<24470)_hv=19;else if(_u<24770)_hv=18; }
+    state.holdblock[3][3][3] = _hv;
+  } else { state.holdblock[3][3][3] = 65; }
   setnextblock();
   setnextblock();
 }
